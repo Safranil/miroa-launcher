48c555654ff7eafca7a24cee5d59052aeeffa544
David Cachau <safranil@safranil.fr>
2016-06-11 23:02:08 +0200

    Add the licence (GPL 3)

--- src/main/resources/safranil/minecraft/miroa/update.php
+++ src/main/resources/safranil/minecraft/miroa/update.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * This file is part of Miroa Launcher.
+ * Copyright (C) 2016 David Cachau <dev@safranil.fr>
+ *
+ * Miroa Launcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Miroa Launcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Miroa Launcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 $CONFIG = [
     "folder_scan" => __DIR__ . "/files",
